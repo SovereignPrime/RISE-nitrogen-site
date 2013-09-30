3af6a11663bfb5801f2e0902b25c531b71b87313
t0ha <t0hashvein@gmail.com>
2013-09-30 13:03:18 +0600

    Main structures added

--- include/db.hrl
+++ include/db.hrl
@@ -1,5 +1,8 @@
 #group{name, subgroups=[]}.
-#contact{name, email, phone, photo, address, groups}.
-#task{}.
-#fiinance{].
-#file{}.
+#contact{name, email, phone, photo, address, groups=[]}.
+#task{id, due, name, text, linked=[]}.
+#file{id, path, type, user, date, status}.
+#expense{id, name, date, type, amount, status}.
+#upd{id, subject, from, text, date}.
+#contact_roles{id, type, role, contct}.
+#attachment{id, file, type, tid}.
