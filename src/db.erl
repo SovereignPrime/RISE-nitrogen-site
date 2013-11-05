7d7d50d3dd718ee4d51bc8882aff22a26338e4cc
t0ha <t0hashvein@gmail.com>
2013-11-05 14:35:50 +0600

    Fixed user creation

--- src/db.erl
+++ src/db.erl
@@ -370,7 +370,7 @@ create_account(User, Passwd, Address) ->
                                                  bitmessage=Address,
                                                  my=Passwd
                                                 }),
-                        mnesia:read(db_contact, N+1);
+                        [U] = mnesia:read(db_contact, N+1);
                     [U] ->
                         U
                 end
