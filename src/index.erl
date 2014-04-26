e649a23262472740dd398b3bc58358eaff28fb73
t0ha <t0hashvein@gmail.com>
2014-04-26 14:43:35 +0600

    Some code reformating

--- src/index.erl
+++ src/index.erl
@@ -17,18 +17,9 @@ icon() -> "<i class='icon-globe icon-2x' style='margin-top:-5px;'></i>".
 buttons(main) -> % {{{1
     #list{class="nav nav-pills", style="display:inline-block;", numbered=false,
           body=[
-%                #listitem{body=[
-%                                %#panel{ class='span2', body="<i class='icon-user'></i> All accounts"},
-%                               ]},
-%                #listitem{body=[
-%                                %#panel{ class='span2', body="<i class='icon-filter'></i> Smart filter"},
-%                               ]},
                 #listitem{body=[
                                 common:render_filters()
                                ]},
-%                #listitem{body=[
-%                                %#panel{ class='span2', body="<i class='icon-sort'></i> Sort"},
-%                               ]},
                 #listitem{body=[
                                 #link{id=archive, body="<i class='icon-list-alt'></i> Archive", postback={show_archive, true}}
                                ]},
@@ -39,16 +30,30 @@ buttons(main) -> % {{{1
 
 left() -> % {{{1
     left(false).
+
 left(Archive) -> % {{{1
-    case db:get_updates(Archive) of % {{{2
+    case db:get_updates(Archive) of 
         {ok, none} ->
             render_left([]);
         {ok, Updates} ->
             render_left(Updates)
     end.
+
 render_left(Updates) -> % {{{1
-    Render = [ #update_preview{id=Id, icon=Enc, from=From, age="Age", subject=Subject, text=Text, flag=true, archive=(Status == archive)} || 
-               #message{hash=Id, from=From, subject=Subject, text=Text, enc=Enc, status=Status} <- sugar:sort_by_timestamp(Updates)],
+    Render = [ #update_preview{id=Id,
+                               icon=Enc,
+                               from=From,
+                               age="Age",
+                               subject=Subject,
+                               text=Text,
+                               flag=true,
+                               archive=(Status == archive)} || 
+               #message{hash=Id,
+                        from=From,
+                        subject=Subject,
+                        text=Text,
+                        enc=Enc,
+                        status=Status} <- sugar:sort_by_timestamp(Updates)],
     #panel{id=left,class="span3 scrollable", body=Render}.
 
 body() -> % {{{1
@@ -56,10 +61,12 @@ body() -> % {{{1
 
 body(Archive) -> % {{{1
     case db:get_updates(Archive) of
-        {ok, []} -> % {{{1
+        {ok, []} -> 
             #panel{id=body, class="span9 scrollable", body=[]};
-        {ok, [ #message{subject=Subject} | _Updates ]} -> % {{{1
-            #panel{id=body, class="span9 scrollable", body=render_body(wf:session_default(current_subject, Subject), Archive)}
+        {ok, [ #message{subject=Subject} | _Updates ]} -> 
+            #panel{id=body,
+                   class="span9 scrollable",
+                   body=render_body(wf:session_default(current_subject, Subject), Archive)}
     end.
 
 render_body(Subject, Archive) -> % {{{1
@@ -68,11 +75,23 @@ render_body(Subject, Archive) -> % {{{1
     [
      #h1{html_encode=false, text="<i class='icon-message'></i> " ++ Subject},
      [
-      #update_element{collapse=true, from=From, to=To, text=Text, uid=Id, subject=Subject, enc=Enc, status=Status} || #message{hash=Id, enc=Enc, to=To, subject=Subject, from=From, text=Text, status=Status} <- sugar:sort_by_timestamp(Updates)
+      #update_element{collapse=true,
+                      from=From,
+                      to=To,
+                      text=Text,
+                      uid=Id,
+                      subject=Subject,
+                      enc=Enc,
+                      status=Status} || #message{hash=Id,
+                                                 enc=Enc,
+                                                 to=To,
+                                                 subject=Subject,
+                                                 from=From,
+                                                 text=Text,
+                                                 status=Status} <- sugar:sort_by_timestamp(Updates)
      ] 
     ].
 
-    
 event({selected, Id, Subject, Archive}) -> % {{{1
     wf:session(current_subject, Subject),
     wf:session(current_update_id, Id),
@@ -80,10 +99,12 @@ event({selected, Id, Subject, Archive}) -> % {{{1
     wf:update(body, render_body(Subject, Archive)),
     wf:wire("$(\".update-preview\").has(\"input[type=checkbox]:checked\").addClass(\"related-message\");"),
     wf:wire("$(\".update-preview\").has(\"input[type=checkbox]:not(:checked)\").removeClass(\"related-message\");");
+
 event({archive, E, Rec}) -> % {{{1
     {ok, #message{subject=Subject}} = db:archive(#message{hash=Rec}),
     wf:replace(left, left()),
     wf:update(body, render_body(Subject, false));
+
 event({show_archive, true}) -> % {{{1
     wf:replace(archive, #link{id=archive, body="<i class='icon-list-alt'></i> Actual", postback={show_archive, false}}),
     {ok, Updates} = db:get_updates(true),
@@ -91,10 +112,12 @@ event({show_archive, true}) -> % {{{1
     {ok, Tasks} = db:get_tasks(true),
     wf:replace(left, render_left(Updates)),
     wf:replace(body, body(true));
+
 event({show_archive, false}) -> % {{{1
     wf:replace(archive, #link{id=archive, body="<i class='icon-list-alt'></i> Archive", postback={show_archive, true}}),
     wf:replace(left, left()),
     wf:replace(body, body());
+
 event(Click) -> % {{{1
     io:format("Event ~p in ~p~n", [Click, ?MODULE]).
 
