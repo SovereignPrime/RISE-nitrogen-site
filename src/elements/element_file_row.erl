21572a5a0a9656c45f1c5e87c38f5ff12ab07c56
t0ha <t0hashvein@gmail.com>
2014-05-14 10:59:38 +0600

    Fixed uploaded size (partly)

--- src/elements/element_file_row.erl
+++ src/elements/element_file_row.erl
@@ -3,6 +3,7 @@
 -module (element_file_row).
 -include_lib("nitrogen_core/include/wf.hrl").
 -include("records.hrl").
+-include("db.hrl").
 -include("torrent.hrl").
 -export([
     reflect/0,
@@ -32,16 +33,16 @@ render_element(Record = #file_row{fid=FID,
     end,
     {ok, Linked} = db:get_linked_messages(FID),
     {Stat, Percent, Uploaded} = case ets:match_object(etorrent_torrent, #torrent{display_name=wf:to_binary(FID), _='_'}) of
-                               [#torrent{state=seeding, all_time_uploaded=U}] ->
+                               [#torrent{state=seeding, uploaded=U, all_time_uploaded=AU}] ->
                                           if Status == downloading ->
                                                  db:mark_downloaded(wf:to_list(FID)),
-                                                 {downloaded, 100, U};
+                                                 {downloaded, 100, AU + U};
                                              Status == uploaded ->
-                                                 {seeding, 100, U};
+                                                 {seeding, 100,AU + U};
                                              true ->
-                                                 {Status, 100, U}
+                                                 {Status, 100, AU + U}
                                           end;
-                               [#torrent{ all_time_uploaded=U, left=Left, total=Total}] ->
+                               [#torrent{ all_time_uploaded=AU, uploaded=U, left=Left, total=Total}] ->
                                         {ok, Pid} = wf:comet(fun() ->
                                                                      receive 
                                                                          update ->
@@ -50,7 +51,7 @@ render_element(Record = #file_row{fid=FID,
                                                                      end
                                                              end),
                                         timer:send_after(10000, Pid, update),
-                                   {downloading, ((Total - Left)  * 100 div Total), U};
+                                   {downloading, ((Total - Left)  * 100 div Total), AU + U};
                                _ ->
                                    {Status, 0, 0}
                            end,
@@ -67,11 +68,40 @@ render_element(Record = #file_row{fid=FID,
             #tablecell{text=Linked, class=""},
             #tablecell{text=sugar:date_format(Date), class=""},
             #tablecell{text=sugar:format_file_size(Uploaded), class=""},
-            %#tablecell{text=Peer, class=""},
             case Stat of
                 downloading ->
-                    #tablecell{body=#progressbar{progress=wf:to_list(Percent), width=80}, class=""};
-                _ ->
-                    #tablecell{text=Stat, class=""}
+                    #tablecell{body=#progressbar{progress=wf:to_list(Percent),
+                                                 width=80},
+                               class=""};
+                _ when Status == uploaded; Status == downloaded ->
+                    [
+                     #tablecell{text=Stat,
+                                class=""},
+                     #tablecell{body=
+                                #panel{class="span1",
+                                       body="<i class='icon icon-save'></i>",
+                                       style="text-align:center;",
+                                       actions=#event{type=click,
+                                                      postback={save, Name, FID},
+                                                      delegate=element_attachment}}}
+                    ];
+                received ->
+                    [
+                     #tablecell{text=Stat,
+                                class=""},
+                     #tablecell{body=
+                                #panel{class="span2",
+                                       body="<i class='icon-download-alt'></i>",
+                                       style="text-align:center;",
+                                       actions=#event{type=click,
+                                                      postback={download, Record},
+                                                      delegate=element_attachment}}}
+                    ]
             end
             ]}.
+
+event({download, #file_row{id=I, fid=Id} = Attachment}) -> % {{{1
+    {ok, [ File ]} = db:get_files([Id]),
+    common:get_torrent(Id), 
+    db:save(File#db_file{status=downloading}),
+    wf:replace(I, Attachment#file_row{status=downloading}).
