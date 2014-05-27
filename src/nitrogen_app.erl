cf08afad2230376021aee135f0d5fe819704b0a0
t0ha <t0hashvein@gmail.com>
2014-05-27 09:30:18 +0600

    Logs to RiseDir

--- src/nitrogen_app.erl
+++ src/nitrogen_app.erl
@@ -21,6 +21,14 @@ start(_StartType, _StartArgs) ->
     application:set_env(etorrent_core, download_dir, RiseDir ++ "/scratch"),
     application:set_env(etorrent_core, dir, RiseDir ++ "/scratch"),
     application:set_env(etorrent_core, fast_resume_file, RiseDir ++ "/fast_resume_state.dets"),
+    application:set_env(sasl, sasl_error_logger, {file, RiseDir ++ "/log/sasl/sasl-error.log"}),
+    application:set_env(lager, handlers,[
+                                         {lager_file_backend,
+                                          [{RiseDir ++ "/log/error.log", error, 10485760, "$D0", 5},
+                                           {RiseDir ++ "/log/console.log", info, 10485760, "$D0", 5},
+                                           {RiseDir ++ "/log/debug.log", debug, 10485760, "$D0", 5}
+                                          ]}
+                                        ]),
     application:start(mnesia),
     application:start(mimetypes),
     application:start(crypto),
